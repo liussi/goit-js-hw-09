39d660a9935927a383e1ab59afb1f8fa2afcda32
liussi <125599487+liussi@users.noreply.github.com>
2023-07-16 20:53:40 +0000

    Deploying to gh-pages from @ liussi/goit-js-hw-09@0cd62b4a6b7d47cd7fbc92ccd14ce8c7b0ee962f 🚀

---
+++ 01-color-switcher.d9bbeb8c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){t.setAttribute("disabled","disabled"),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled","disabled"),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.d9bbeb8c.js.map
