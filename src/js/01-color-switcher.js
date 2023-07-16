0cd62b4a6b7d47cd7fbc92ccd14ce8c7b0ee962f
Liudmila-Ridko <125599487+liussi@users.noreply.github.com>
2023-07-16 23:52:43 +0300

    numer1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const COLOR_DELAY = 1000;
+let id = null;
+
+startBtn.addEventListener('click',onColorInterval );
+stopBtn.addEventListener('click', onRemoveInterval);
+
+function onColorInterval (){
+    startBtn.setAttribute("disabled", "disabled");
+
+    id = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+    },COLOR_DELAY);
+}
+
+function onRemoveInterval () {
+    startBtn.removeAttribute("disabled", "disabled")
+    clearInterval(id);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
