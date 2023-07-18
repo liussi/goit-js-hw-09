8f2e0d1f940400192231809b82ca6ee904573e1d
Liudmila-Ridko <125599487+liussi@users.noreply.github.com>
2023-07-18 23:06:05 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,19 +3,15 @@ import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
 const daysEl = document.querySelector('span[data-days]');
-console.log(daysEl)
 const hoursEl = document.querySelector('span[data-hours]');
-console.log(hoursEl);
 const minutesEl = document.querySelector('span[data-minutes]');
-console.log(minutesEl);
-
 const secondsEl = document.querySelector('span[data-seconds]');
 const startBtn = document.querySelector('button[data-start]');
 const input = document.querySelector('#datetime-picker');
 
 startBtn.addEventListener('click', inervalId)
-
 startBtn.setAttribute("disabled", "disabled");
+
 const dateNow = Date.now();
  
 const options = {
@@ -32,20 +28,22 @@ const options = {
         }  
     },
 };
+
 const fp =  flatpickr(input, options);
 
 function inervalId(){ 
     setInterval(() => {
+
         const currentDate = Date.now();
         let ms = fp.selectedDates[0].getTime() - currentDate;
+
     if(ms < 0){
         return
     }
     convertMs(ms);
+
 },1000);
-
-}
-
+};
 
 function convertMs(ms) {
     
@@ -64,18 +62,12 @@ const second = 1000;
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
     
     
-    daysEl.texContent = addLeadingZero(days);
-    console.log(daysEl.texContent);
-
-    hoursEl.texContent = addLeadingZero(hours);
-    console.log(hoursEl.texContent);
-    minutesEl.textContent = addLeadingZero(minutes);
-    secondsEl.textContent = addLeadingZero(seconds);
-    
-
-    
-    return { days, hours, minutes, seconds };
-   
+daysEl.textContent = addLeadingZero(days);
+hoursEl.textContent = addLeadingZero(hours);
+minutesEl.textContent = addLeadingZero(minutes);
+secondsEl.textContent = addLeadingZero(seconds);
+     
+    return { days, hours, minutes, seconds }; 
 }  
 
 function addLeadingZero(value) {
